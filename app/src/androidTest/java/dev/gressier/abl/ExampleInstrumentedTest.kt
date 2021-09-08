080688160cc1512b90cfa0a9c5996c66425c781d
Alexandre Gressier <alexandre@gressier.dev>
2021-09-09 00:09:41 +0200

    Reformat the default project files

--- app/src/androidTest/java/dev/gressier/abl/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/dev/gressier/abl/ExampleInstrumentedTest.kt
@@ -8,16 +8,9 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
+
+    @Test fun useAppContext() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("dev.gressier.abl", appContext.packageName)
     }
--- app/src/test/java/dev/gressier/abl/ExampleUnitTest.kt
+++ app/src/test/java/dev/gressier/abl/ExampleUnitTest.kt
@@ -4,14 +4,9 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
+
+    @Test fun `Addition is correct`() {
         assertEquals(4, 2 + 2)
     }
 }
