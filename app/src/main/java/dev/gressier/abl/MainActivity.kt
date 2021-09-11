370cdf216530917fd3caef456ec4d033a9f278d9
Alexandre Gressier <alexandre@gressier.dev>
2021-09-11 22:18:44 +0200

    Implement the navigation drawer

--- app/src/main/java/dev/gressier/abl/MainActivity.kt
+++ app/src/main/java/dev/gressier/abl/MainActivity.kt
@@ -2,12 +2,40 @@ package dev.gressier.abl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import dev.gressier.abl.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Title
         title = getString(R.string.app_name)
+
+        // Layout
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Navigation
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.containerFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.navView.setupWithNavController(navController)
+
+        // App Bar
+        setSupportActionBar(binding.toolbar)
+        appBarConfiguration = AppBarConfiguration(binding.navView.menu, binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean =
+        ::navController.isInitialized && navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 }
