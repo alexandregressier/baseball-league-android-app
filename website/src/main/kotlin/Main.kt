7d31faf449dc72851b05d7b945eeb5dc34b6ec94
Alexandre Gressier <alexandre@gressier.dev>
2021-09-15 11:04:34 +0200

    Rewrite the ABL website with function components

--- website/src/main/kotlin/Main.kt
+++ website/src/main/kotlin/Main.kt
@@ -1,38 +1,177 @@
 import kotlinx.browser.document
+import kotlinx.css.*
+import kotlinx.css.properties.TextDecoration
 import react.Props
 import react.RBuilder
-import react.dom.*
+import react.dom.p
+import react.dom.render
 import react.fc
 import react.router.dom.*
-import styled.injectGlobal
+import styled.*
+import teams.Team
+import teams.TeamRepository
+import teams.TeamRepositoryStatic
 
-object UrlPrefix {
-    const val team = "/teams"
+object Path {
+    const val teams = "/teams"
+    const val root = "/"
 }
 
-val Teams = fc<Props> {
-    div {
-        h2 { +"Teams" }
-        ul {
-            li { Link { attrs.to = "${UrlPrefix.team}/abc"; +"ABC" } }
-            li { Link { attrs.to = "${UrlPrefix.team}/def"; +"DEF" } }
-            li { Link { attrs.to = "${UrlPrefix.team}/xyz"; +"XYZ" } }
-            li { Link { attrs.to = UrlPrefix.team; +"None" } }
+val teamRepository: TeamRepository = TeamRepositoryStatic()
+
+val TeamDetails = fc<Props> {
+    val teamId = useParams()["teamId"] ?: return@fc
+    val team = teamRepository.getTeamById(teamId)
+
+    team?.apply {
+        styledDiv {
+            css {
+                marginTop = 7.vh
+            }
+            styledDiv {
+                css {
+                    width = 17.rem
+                    margin = "auto"
+                    textAlign = TextAlign.center
+                }
+                styledImg(src = "/img/team-logos/${id.lowercase()}.svg", alt = "$city $name Logo") {
+                    css {
+                        display = Display.inlineBlock
+                        width = 14.rem
+                        height = 14.rem
+                        padding = "1.2rem"
+                        margin = "auto"
+                        backgroundColor = Color.white
+                        borderRadius = 3.rem
+                    }
+                }
+                p { +"$city $name" }
+            }
+        }
+        styledDiv {
+            css {
+                margin = "10vw 0"
+                textAlign = TextAlign.center
+            }
+            Link { attrs.to = Path.root
+                styledSpan {
+                    css {
+                        display = Display.inlineBlock
+                        width = 8.rem
+                        padding = "1rem"
+                        color = Color.black
+                        textDecoration = TextDecoration.none
+                        textAlign = TextAlign.center
+                        fontWeight = FontWeight.bold
+                        backgroundColor = Color.white
+                        borderRadius = 1.rem
+                    }
+                    +"< Back"
+                }
+            }
         }
     }
 }
-val Team = fc<Props> {
-    val teamId = useParams()["teamId"] ?: return@fc
-    h3 { +"Requested team ID: $teamId" }
+
+fun RBuilder.TeamDetails(handler: Props.() -> Unit) =
+    child(TeamDetails) { attrs { handler() } }
+
+
+external interface TeamCardProps : Props {
+    var team: Team
 }
+
+val TeamCard = fc<TeamCardProps> { props ->
+    props.team.apply {
+        styledDiv {
+            css {
+                width = 5.rem
+                margin = "0.8rem"
+                textAlign = TextAlign.center
+            }
+            styledImg(src = "/img/team-logos/${id.lowercase()}.svg", alt = "$city $name Logo") {
+                css {
+                    display = Display.inlineBlock
+                    width = 4.rem
+                    height = 4.rem
+                    padding = "0.4rem"
+                    margin = "auto"
+                    backgroundColor = Color.white
+                    borderRadius = 1.rem
+                }
+            }
+            styledP {
+                css {
+                    fontSize = 0.7.rem
+                    margin = "0 10px"
+                }
+                +"$city $name"
+            }
+        }
+    }
+}
+
+fun RBuilder.TeamCard(handler: TeamCardProps.() -> Unit) =
+    child(TeamCard) { attrs { handler() } }
+
+
+external interface TeamCardsProps : Props {
+    var teams: List<Team>
+}
+
+val TeamCards = fc<TeamCardsProps> { props ->
+    styledDiv {
+        css {
+            display = Display.flex
+            justifyContent = JustifyContent.center
+            flexWrap = FlexWrap.wrap
+            maxWidth = 1000.px
+            margin = "auto"
+            paddingTop = 3.vh
+        }
+        teamRepository.getTeams().forEach { t ->
+            Link { attrs.to = t.run { "${Path.teams}/$id?teamName=$city $name" }
+                TeamCard { team = t }
+            }
+        }
+    }
+}
+
+fun RBuilder.TeamCards(handler: TeamCardsProps.() -> Unit) =
+    child(TeamCards) { attrs { handler() } }
+
 
 fun RBuilder.appWithRouter() {
     BrowserRouter {
-        h1 { Link { attrs.to = "/"; +"Android Baseball League Teams" } }
+        styledHeader {
+            css {
+                position = Position.sticky
+                top = 0.px
+                textAlign = TextAlign.center
+                padding = "1rem"
+                backgroundColor = Color.white
+            }
+            styledH1 {
+                css {
+                    margin = "0"
+                    fontSize = 1.8.rem
+                }
+                Link { attrs.to = Path.root; +"Android Baseball League Teams" }
+            }
+        }
         Switch {
-            Route { attrs { path = arrayOf("${UrlPrefix.team}/:teamId"); component = Team } }
-            Redirect { attrs { from = UrlPrefix.team; to = "/" } }
-            Route { attrs { path = arrayOf("/"); component = Teams } }
+            Route { attrs { path = arrayOf("${Path.teams}/:teamId"); component = TeamDetails } }
+            Redirect { attrs { from = Path.teams; to = Path.root } }
+            Route { attrs { path = arrayOf(Path.root); component = TeamCards } }
+        }
+        styledFooter {
+            css {
+                padding = "1rem 0"
+                color = Color.grey
+                fontSize = 0.7.rem
+                textAlign = TextAlign.center
+            }
+            +"Copyright &copy; 2021"
         }
     }
 }
--- website/src/main/kotlin/teams/TeamRepository.kt
+++ website/src/main/kotlin/teams/TeamRepository.kt
@@ -1,26 +1,38 @@
 package teams
 
+import teams.Team.Division.EAST
+import teams.Team.Division.WEST
+
 interface TeamRepository {
+    fun getTeamById(id: TeamId): Team?
     fun getTeams(): List<Team>
 }
 
 class TeamRepositoryStatic : TeamRepository {
 
+    override fun getTeamById(id: TeamId): Team? =
+        teams.firstOrNull { it.id == id }
+
     override fun getTeams(): List<Team> =
-        listOf(
-            Team("APL", "Appleton", "Foxes", Team.Division.EAST),
-            Team("BOO", "Baraboo", "Big Tops", Team.Division.WEST),
-            Team("EC", "Eau Claire", "Blues", Team.Division.WEST),
-            Team("GB", "Green Bay", "Skyline", Team.Division.EAST),
-            Team("LAX", "La Crosse", "Lovers", Team.Division.WEST),
-            Team("LD", "Lake Delton", "Breakers", Team.Division.WEST),
-            Team("MSN", "Madison", "Snowflakes", Team.Division.WEST),
-            Team("MKE", "Milwaukee", "Sunrise", Team.Division.EAST),
-            Team("PKE", "Pewaukee", "Princesses", Team.Division.EAST),
-            Team("SHW", "Shawano", "Shorebirds", Team.Division.EAST),
-            Team("SG", "Spring Green", "Thespians", Team.Division.WEST),
-            Team("SB", "Sturgeon Bay", "Elders", Team.Division.EAST),
-            Team("WAU", "Waukesha", "Riffs", Team.Division.EAST),
-            Team("WR", "Wisconsin Rapids", "Cranberries", Team.Division.WEST),
-        )
+        teams
+
+    companion object {
+        private val teams =
+            listOf(
+                Team("APL", "Appleton", "Foxes", EAST),
+                Team("BOO", "Baraboo", "Big Tops", WEST),
+                Team("EC", "Eau Claire", "Blues", WEST),
+                Team("GB", "Green Bay", "Skyline", EAST),
+                Team("LAX", "La Crosse", "Lovers", WEST),
+                Team("LD", "Lake Delton", "Breakers", WEST),
+                Team("MSN", "Madison", "Snowflakes", WEST),
+                Team("MKE", "Milwaukee", "Sunrise", EAST),
+                Team("PKE", "Pewaukee", "Princesses", EAST),
+                Team("SHW", "Shawano", "Shorebirds", EAST),
+                Team("SG", "Spring Green", "Thespians", WEST),
+                Team("SB", "Sturgeon Bay", "Elders", EAST),
+                Team("WAU", "Waukesha", "Riffs", EAST),
+                Team("WR", "Wisconsin Rapids", "Cranberries", WEST),
+            )
+    }
 }
